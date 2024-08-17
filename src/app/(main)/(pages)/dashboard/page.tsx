41e3bc94c8f70fcb3cecff50b54c9c65666ac4fd
MarselDokubo <connectmarsel@gmail.com>
2024-08-18 00:54:55 +0100

    chore(component+): dashboard

--- src/app/(main)/(pages)/dashboard/page.tsx
+++ src/app/(main)/(pages)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const DashboardPage = () => {
+const Dashboard: React.FunctionComponent = () => {
   return (
     <div className="flex flex-col gap-4 relative">
       <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b">
@@ -10,4 +10,4 @@ const DashboardPage = () => {
   );
 };
 
-export default DashboardPage;
+export default Dashboard;
