d4997efe17204b69104eebecf054bc9695cba276
MarselDokubo <connectmarsel@gmail.com>
2024-08-20 02:41:25 +0100

    chore(page+): settings

--- src/components/global/navbar.tsx
+++ src/components/global/navbar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { MenuIcon } from "lucide-react";
+import { UserButton } from "@clerk/nextjs";
 
 type Props = {};
 
@@ -51,6 +52,7 @@ const Navbar = async (props: Props) => {
             Get Started
           </span>
         </Link>
+        <UserButton />
         <MenuIcon className="md:hidden" />
       </aside>
     </header>
